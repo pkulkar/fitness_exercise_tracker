100567a41814818963184906902b82b28014ff5a
Pranit Kulkarni <pkulkar2@binghamton.edu>
2020-04-17 14:00:23 -0700

    Created exercise and user models

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
-
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
@@ -20,8 +18,11 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
-
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
